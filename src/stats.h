cf13d9e31605a68d8b0f70e62bfe83da74cf3a27
gamemann <gamemann@gflclan.com>
2020-11-22 12:50:24 +0000

    Remove timestamp checking for PPS since we'll be sleeping every second anyways.

--- src/stats.h
+++ src/stats.h
@@ -1,6 +1,5 @@
 #pragma once
 
 #include <inttypes.h>
-#include <time.h>
 
-void dopps(const char *interface, uint64_t *totpckts, time_t *lastupdate);
+void dopps(const char *interface, uint64_t *totpckts);
